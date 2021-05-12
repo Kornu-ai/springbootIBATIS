9044013dff2b18179282e55ab2835c8bee56ce71
babji <babji@DESKTOP-2VFRJ0D>
2021-05-12 15:52:08 +0530

    intial commit

---
+++ SpringbootIBATIS/src/main/resources/snippet/Snippet.java
@@ -0,0 +1,13 @@
+package snippet;
+
+public class Snippet {
+	create table employees
+	(
+	   id integer not null,
+	   first_name varchar(255) not null, 
+	   last_name varchar(255) not null,
+	   email_address varchar(255) not null,
+	   primary key(id)
+	);
+}
+
